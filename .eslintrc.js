d45075e2588a12397636e3980030150b402ad5ee
Alex Chapp <alex.chapp@gambyt.com>
2021-10-24 17:59:05 -0400

    Use ts, add prettier, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module' // Allows for the use of imports
+  },
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      1,
+      {
+        argsIgnorePattern: '^_'
+      }
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/type-annotation-spacing': 1,
+    '@typescript-eslint/no-empty-function': 'off',
+    curly: 1,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    'prettier/prettier': 'off' //Don't enforce prettier through ESLint -- causes a lot of really ugly warnings/errors when editing. Set your IDE to auto-format on save using prettier.
+  }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import { SITE_TITLE } from '../components/util/constants'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>{SITE_TITLE}</title>
+      </Head>
+        <div>Hello World</div>
+    </>
+  )
+}
