249540c552efd0d7707155efea1b3449d1e63dd0
chappstick9 <chapp@umich.edu>
2021-10-27 23:16:24 -0400

    Attempt to add basic auth0 login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import Head from 'next/head'
 import { SITE_TITLE } from '../components/util/constants'
+import { useUser } from '@auth0/nextjs-auth0'
 
 export default function Home() {
+  const { user, error, isLoading } = useUser()
+
+  const loginLogout = user ? (
+    <div>
+      Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+    </div>
+  ) : (
+    <a href="/api/auth/login">Login</a>
+  )
+
   return (
     <>
       <Head>
@@ -9,6 +20,7 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <div>Hello World! This is MemoryBook</div>
+      {loginLogout}
     </>
   )
 }
